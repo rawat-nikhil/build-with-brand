5376474476c834e4c23449dbb8fd4f7665d6b7b5
Nikhil Rawat <nikhil.rawat@tothenew.com>
2026-08-16 09:53:20 +0530

    feat: add team cards

--- src/content/team.ts
+++ src/content/team.ts
@@ -1,8 +1,12 @@
 import type { TeamMember } from "@/types";
 
 export const team: TeamMember[] = [
-  { name: "Nikhil Rawat", role: "Founder & CEO" },
-  { name: "Aastha Negi", role: "UI/UX Designer" },
-  { name: "Ankit Bisht", role: "Full Stack Developer" },
-  { name: "Mayank Joshi", role: "Front End Developer" },
+  { name: "Nikhil Rawat", role: "Software Developer" },
+  { name: "Arushi Negi", role: "Product Manager" },
+  { name: "Abhinav Kathait", role: "Front End Developer" },
+  { name: "Tushar Badola", role: "Full Stack Developer" },
+  { name: "Himanshu Khali", role: "Full Stack Developer" },
+  { name: "Rakesh Chauhan", role: "DevOps" },
+  { name: "Ashish Purohit", role: "Backend Developer" },
+  { name: "Akshay Purohit", role: "UI/UX Designer" },
 ];
